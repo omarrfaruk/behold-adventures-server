f601bd9f6708c3f70b0d69bd762fe63de70cd7b6
jankar-with-tarek <avirus170@gmail.com>
2022-08-30 10:23:59 +0600

    added dashboard

--- controllers/tour.js
+++ controllers/tour.js
@@ -44,3 +44,42 @@ export const getTourByUser = async (req, res) => {
     const userTours = await TourModal.find({ creator: id })
     res.status(200).json(userTours)
 }
+
+export const deleteTour = async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(404).json({ message: 'No Tour Exist With this User' })
+        }
+
+        await TourModal.findByIdAndRemove(id)
+        res.json({ message: 'Tour Deleted Successfully' })
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+    }
+}
+
+export const updateTour = async (req, res) => {
+    const { id } = req.params;
+    const { title, imageFile, tags, creator, description } = req.body;
+
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(404).json({ message: 'No Tour Exist With this User' })
+        }
+
+        const updatedTour = {
+            title,
+            imageFile,
+            tags,
+            creator,
+            description,
+            _id: id
+        }
+
+        await TourModal.findByIdAndUpdate(id, updatedTour, { new: true })
+        res.json(updatedTour)
+    } catch (error) {
+        res.status(404).json({ message: 'Something went wrong' })
+    }
+}
--- routes/tour.js
+++ routes/tour.js
@@ -1,13 +1,15 @@
 import express from "express"
 const router = express.Router()
 
-import { createTour, getTour, getTourByUser, getTours } from "../controllers/tour.js"
+import { createTour, deleteTour, getTour, getTourByUser, getTours, updateTour } from "../controllers/tour.js"
 import auth from "../middleware/auth.js";
 
 
 router.post('/', auth, createTour);
 router.get('/', getTours);
 router.get('/:id', getTour);
+router.delete('/:id', auth, deleteTour);
+router.patch('/:id', auth, updateTour);
 router.get('/userTours/:id', getTourByUser);
 
 export default router;
