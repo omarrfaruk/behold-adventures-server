144ec42f8dc55e66f7e6e9e2be8880aadf3b4414
jankar-with-tarek <avirus170@gmail.com>
2022-08-30 18:09:48 +0600

    added search functon

--- routes/tour.js
+++ routes/tour.js
@@ -1,15 +1,18 @@
 import express from "express"
 const router = express.Router()
 
-import { createTour, deleteTour, getTour, getTourByUser, getTours, updateTour } from "../controllers/tour.js"
+import { createTour, deleteTour, getTour, getToursBySearch, getTourByUser, getTours, updateTour } from "../controllers/tour.js"
 import auth from "../middleware/auth.js";
 
 
-router.post('/', auth, createTour);
 router.get('/', getTours);
+router.get("/search", getToursBySearch);
 router.get('/:id', getTour);
+
+
+router.post('/', auth, createTour);
 router.delete('/:id', auth, deleteTour);
 router.patch('/:id', auth, updateTour);
-router.get('/userTours/:id', getTourByUser);
+router.get('/userTours/:id', auth, getTourByUser);
 
 export default router;
