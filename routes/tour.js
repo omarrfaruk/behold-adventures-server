c16422db7efb78901884a9d0091ed9457c0f5cff
jankar-with-tarek <avirus170@gmail.com>
2022-09-06 16:30:39 +0600

    added related tour

--- routes/tour.js
+++ routes/tour.js
@@ -1,13 +1,14 @@
 import express from "express"
 const router = express.Router()
 
-import { createTour, deleteTour, getTour, getToursBySearch, getTourByUser, getTours, updateTour, getToursByTags } from "../controllers/tour.js"
+import { createTour, deleteTour, getTour, getRelatedTours, getToursBySearch, getTourByUser, getTours, updateTour, getToursByTags } from "../controllers/tour.js"
 import auth from "../middleware/auth.js";
 
 
 router.get('/', getTours);
 router.get("/search", getToursBySearch);
 router.get("/tag/:tag", getToursByTags);
+router.post("/relatedTours", getRelatedTours);
 router.get('/:id', getTour);
 
 
